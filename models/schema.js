7d1fbb3b2b3941edad03b30914f3e24eadd344ea
kevocs410 <kevocs410@yahoo.com>
2022-08-12 13:02:42 -0400

    all routes made

---
+++ models/schema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const animeSchema = new Schema({
+    name: String,
+    img: {type: String, required: false},
+    year:Number,
+    genre: String,
+    description:String
+});
+
+const animeCollection = mongoose.model('Anime', animeSchema)
+
+module.exports = animeCollection
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
+const Anime= require('./models/schema.js');
 const app = express ();
 const db = mongoose.connection;
 require('dotenv').config()
@@ -44,6 +45,8 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
+///// model
+
 //___________________
 // Routes
 //___________________
@@ -54,15 +57,26 @@ app.get('/' , (req, res) => {
 
 /// create route 
 app. get("/anime/new", (req,res)=>{
-  res.send("create page")
+  res.render("new.ejs")
   // res.render("new.ejs");
 })
 
+app.post("/anime", (req, res) =>{
+
+  Anime.create(req.body, (error,createdAnime) =>{
+    res.redirect("/anime")
+  });
+});
 
 
 
 ///show route
 app.get("/anime/:id", (req, res)=>{
+  Anime.findById(req.params.id, (err,foundAnime) =>{
+    res.render("show.ejs",{
+      animes:foundAnime
+    })
+  })
   res.send("show page")
   // res.render("show.ejs")
 })
@@ -70,18 +84,47 @@ app.get("/anime/:id", (req, res)=>{
 
 
 ////index route
-app.get("/anime", (req,res)=>{
-  res.send("Hello Start Page")
-})
+app.get('/smash', (req, res) => {
+  Anime.find({}, (error, allAnimes) => {
+      res.render('index.ejs', {
+          animes: allAnimes
+      });
+  });
+});
 
 ///delete route
-
+app.delete('/anime/:id', (req, res)=>{
+  // res.send('deleting...');
+  Anime.findByIdAndRemove(req.params.id, (err, removedAnime) => {
+      res.redirect('/anime');
+  });
+});
 
 
 ////edit route
-app.get("/anime/edit", (req,res)=>{
-  res.send("edit page")
-})
+app.get('/anime/:id/edit', (req, res)=>{
+  Anime.findById(req.params.id, (err, foundAnime)=>{
+      res.render(
+      'edit.ejs',
+      {
+        animes: foundAnime
+      }
+    );
+  });
+});
+
+
+
+//////UPDATED ROUTES
+
+
+app.put('/anime/:id', (req, res)=>{
+  Anime.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedAnime) => {
+      res.redirect('/anime');
+  });
+});
+
+
 
 
 //___________________
